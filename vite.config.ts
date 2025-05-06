5169974beba987dd8f61546bd2ed8c21af7523ef
Pathum-SC12402 <fernando12402@usci.ruh.ac.lk>
2025-05-06 14:19:00 +0530

    added test file and changed test.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    ...configDefaults,
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
